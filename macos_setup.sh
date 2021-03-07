2a60e9b97d509ccd167d03edba2424d1be9d9508
Jonathan Yang <cjyk84719@gmail.com>
2021-03-07 10:27:59 +0100

    update system setup

--- macos_setup.sh
+++
@@ -1,5 +0,0 @@
-# Keep folders on top when sorting by name
-defaults write com.apple.finder _FXSortFoldersFirst -bool true
-
-# Show path bar.
-defaults write com.apple.finder ShowPathbar -bool true
